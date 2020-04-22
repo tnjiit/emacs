df06da8adf893e7ec6960bb83e4c27a00dc288c8
Tushar N K Jain <tusharjain@fb.com>
2020-04-22 16:32:28 -0700

    Latest pull

--- elpa/treemacs-magit-20200302.553/treemacs-magit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-magit" "20200302.553" "Magit integration for treemacs" '((emacs "25.2") (treemacs "0.0") (pfuture "1.3") (magit "2.90.0")) :commit "f82d61bdcb4ce4b294a95628ce1d62857991df48" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
