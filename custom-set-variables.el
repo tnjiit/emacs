11d9f673925c056056fa328ec09371b84286e224
tusharjain <tnjiitr@gmail.com>
2020-10-04 23:56:35 -0700

    Upgrading packages.

--- custom-set-variables.el
+++ custom-set-variables.el
@@ -76,17 +76,17 @@
  '(ccls-enable-skipped-ranges nil t)
  '(ccls-executable nil t)
  '(ccls-sem-highlight-method (quote font-lock) t)
- '(company-begin-commands (quote (self-insert-command)))
+ '(company-begin-commands (quote (self-insert-command)) t)
  '(company-box-backends-colors nil t)
  '(company-box-doc-delay 0.3)
  '(company-box-max-candidates 50)
  '(company-box-show-single-candidate t)
- '(company-global-modes (quote (not shell-mode eaf-mode)))
- '(company-idle-delay 0.1)
- '(company-minimum-prefix-length 1)
- '(company-require-match (quote never))
- '(company-show-numbers t)
- '(company-tooltip-align-annotations t)
+ '(company-global-modes (quote (not shell-mode eaf-mode)) t)
+ '(company-idle-delay 0.1 t)
+ '(company-minimum-prefix-length 1 t)
+ '(company-require-match (quote never) t)
+ '(company-show-numbers t t)
+ '(company-tooltip-align-annotations t t)
  '(csv-header-lines 1)
  '(custom-safe-themes
    (quote
@@ -96,37 +96,34 @@
  '(dashboard-navigator-buttons
    (quote
     (((#("" 0 1
-         (face
-          (:family "github-octicons" :height 1.32)
-          font-lock-face
-          (:family "github-octicons" :height 1.32)
-          display
-          (raise -0.06)
-          rear-nonsticky t))
+         (rear-nonsticky t display
+                         (raise -0.06)
+                         font-lock-face
+                         (:family "github-octicons" :height 1.32)
+                         face
+                         (:family "github-octicons" :height 1.32)))
        "M-EMACS" "Browse M-EMACS Homepage"
        (lambda
          (&rest _)
          (browse-url "https://github.com/MatthewZMD/.emacs.d")))
       (#("" 0 1
-         (face
-          (:family "file-icons" :height 1.2)
-          font-lock-face
-          (:family "file-icons" :height 1.2)
-          display
-          (raise -0.12)
-          rear-nonsticky t))
+         (rear-nonsticky t display
+                         (raise -0.12)
+                         font-lock-face
+                         (:family "file-icons" :height 1.2)
+                         face
+                         (:family "file-icons" :height 1.2)))
        "Configuration" ""
        (lambda
          (&rest _)
          (edit-configs)))
       (#("" 0 1
-         (face
-          (:family "FontAwesome" :height 1.2)
-          font-lock-face
-          (:family "FontAwesome" :height 1.2)
-          display
-          (raise -0.12)
-          rear-nonsticky t))
+         (rear-nonsticky t display
+                         (raise -0.12)
+                         font-lock-face
+                         (:family "FontAwesome" :height 1.2)
+                         face
+                         (:family "FontAwesome" :height 1.2)))
        "Update" ""
        (lambda
          (&rest _)
@@ -175,7 +172,7 @@
    (quote
     (not text-mode outline-mode fundamental-mode org-mode diff-mode shell-mode eshell-mode term-mode)))
  '(flycheck-indication-mode (quote right-fringe))
- '(flycheck-pos-tip-timeout 30)
+ '(flycheck-pos-tip-timeout 30 t)
  '(flycheck-python-pycompile-executable "python3")
  '(global-auto-revert-mode t)
  '(global-auto-revert-non-file-buffers t)
@@ -191,7 +188,7 @@
  '(grep-template "grep <X> <C> -nH -e <R> <F>")
  '(grep-use-null-device nil)
  '(header-copyright-notice "Copyright (C) 2019 Tushar Jain
-")
+" t)
  '(hl-sexp-background-color "#1c1f26")
  '(ibuffer-formats
    (quote
@@ -204,7 +201,7 @@
            " " filename-and-process)
      (mark " "
            (name 16 -1)
-           " " filename))) t)
+           " " filename))))
  '(ibuffer-vc-skip-if-remote nil t)
  '(inhibit-compacting-font-caches t t)
  '(ivy-count-format "【%d/%d】")
@@ -223,18 +220,18 @@
  '(menu-bar-mode t)
  '(multi-term-program "/bin/bash" t)
  '(objed-cursor-color "#ff6c6b")
- '(org-agenda-window-setup (quote other-window) t)
- '(org-confirm-babel-evaluate nil t)
- '(org-export-backends (quote (ascii html icalendar latex md odt)) t)
- '(org-log-done (quote time) t)
+ '(org-agenda-window-setup (quote other-window))
+ '(org-confirm-babel-evaluate nil)
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
+ '(org-log-done (quote time))
  '(org-todo-keywords
    (quote
-    ((sequence "TODO" "IN-PROGRESS" "REVIEW" "|" "DONE"))) t)
- '(org-use-speed-commands t t)
+    ((sequence "TODO" "IN-PROGRESS" "REVIEW" "|" "DONE"))))
+ '(org-use-speed-commands t)
  '(p4-verbose nil)
  '(package-selected-packages
    (quote
-    (jetbrains-darcula-theme ein pyvenv elpy importmagic indent-tools py-autopep8 py-yapf pydoc auto-complete auto-complete-auctex auto-complete-c-headers auto-complete-chunk auto-complete-clang auto-complete-clang-async auto-complete-distel auto-complete-exuberant-ctags auto-complete-nxml auto-complete-pcmp auto-complete-rst auto-complete-sage bbyac compact-docstrings company-anaconda company-ansible company-arduino company-auctex company-axiom company-bibtex company-c-headers company-cabal company-coq company-ctags company-dcd company-dict company-distel company-ebdb company-edbi company-emacs-eclim company-emoji company-erlang company-flow company-flx company-fuzzy company-ghc company-ghci company-glsl company-go company-inf-ruby company-irony company-irony-c-headers company-jedi company-lean company-lua company-math company-nand2tetris company-native-complete company-nginx company-ngram company-nixos-options company-org-roam company-php company-phpactor company-plsense company-pollen company-posframe company-prescient company-qml company-quickhelp company-quickhelp-terminal company-racer company-reftex company-restclient company-rtags company-shell company-solidity company-sourcekit company-stan company-statistics company-suggest company-tern company-terraform company-try-hard company-web company-ycm company-ycmd groovy-mode dockerfile-mode docker auctex haskell-mode company lsp-mode flycheck-posframe flycheck-grammarly yasnippet treemacs projectile magit lsp-pyre ivy-rich ivy-posframe yasnippet-snippets which-key web-mode use-package undo-tree typescript-mode treemacs-projectile treemacs-magit toc-org term-keys super-save sudo-edit speed-type smartparens shell-here quickrun pyim posframe popup-kill-ring plantuml-mode pdf-tools ox-gfm org-edit-latex mu4e-overview mu4e-alert modern-cpp-font-lock lsp-ui lsp-python-ms lsp-java json-mode js2-mode iedit htmlize highlight-indent-guides graphql go-mode format-all flycheck exec-path-from-shell evil-nerd-commenter ess erc-image erc-hl-nicks emmet-mode dumb-jump doom-themes doom-modeline disk-usage discover-my-major dired-single diminish dashboard dap-mode crux counsel company-tabnine company-lsp company-box ccls beacon auto-package-update amx all-the-icons-dired aio 2048-game)))
+    (flycheck-pos-tip jetbrains-darcula-theme ein pyvenv elpy importmagic indent-tools py-autopep8 py-yapf pydoc auto-complete auto-complete-auctex auto-complete-c-headers auto-complete-chunk auto-complete-clang auto-complete-clang-async auto-complete-distel auto-complete-exuberant-ctags auto-complete-nxml auto-complete-pcmp auto-complete-rst auto-complete-sage bbyac compact-docstrings company-anaconda company-ansible company-arduino company-auctex company-axiom company-bibtex company-c-headers company-cabal company-coq company-ctags company-dcd company-dict company-distel company-ebdb company-edbi company-emacs-eclim company-emoji company-erlang company-flow company-flx company-fuzzy company-ghc company-ghci company-glsl company-go company-inf-ruby company-irony company-irony-c-headers company-jedi company-lean company-lua company-math company-nand2tetris company-native-complete company-nginx company-ngram company-nixos-options company-org-roam company-php company-phpactor company-plsense company-pollen company-posframe company-prescient company-qml company-quickhelp company-quickhelp-terminal company-racer company-reftex company-restclient company-rtags company-shell company-solidity company-sourcekit company-stan company-statistics company-suggest company-tern company-terraform company-try-hard company-web company-ycm company-ycmd groovy-mode dockerfile-mode docker auctex haskell-mode company lsp-mode flycheck-posframe flycheck-grammarly yasnippet treemacs projectile magit lsp-pyre ivy-rich ivy-posframe yasnippet-snippets which-key web-mode use-package undo-tree typescript-mode treemacs-projectile treemacs-magit toc-org term-keys super-save sudo-edit speed-type smartparens shell-here quickrun pyim posframe popup-kill-ring plantuml-mode pdf-tools ox-gfm org-edit-latex mu4e-overview mu4e-alert modern-cpp-font-lock lsp-ui lsp-python-ms lsp-java json-mode js2-mode iedit htmlize highlight-indent-guides graphql go-mode format-all flycheck exec-path-from-shell evil-nerd-commenter ess erc-image erc-hl-nicks emmet-mode dumb-jump doom-themes doom-modeline disk-usage discover-my-major dired-single diminish dashboard dap-mode crux counsel company-tabnine company-lsp company-box ccls beacon auto-package-update amx all-the-icons-dired aio 2048-game)))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(projectile-completion-system (quote ivy) t)
  '(pyim-default-scheme (quote quanpin) t)
@@ -256,7 +253,7 @@
  '(show-paren-mode t)
  '(show-trailing-whitespace t)
  '(size-indication-mode t)
- '(sp-escape-quotes-after-insert nil)
+ '(sp-escape-quotes-after-insert nil t)
  '(super-save-auto-save-when-idle nil nil nil "Customized with use-package super-save")
  '(super-save-mode nil)
  '(term-bind-key-alist
--- elpa/lsp-python-ms-20200907.815/lsp-python-ms-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-python-ms" "20200907.815" "The lsp-mode client for Microsoft python-language-server" '((emacs "25.1") (lsp-mode "6.0")) :commit "5fde835928dba36531f5d80112cfd4ce70339da9" :keywords '("languages" "tools") :authors '(("Charl Botha")) :maintainer '("Andrew Christianson, Vincent Zhang") :url "https://github.com/emacs-lsp/lsp-python-ms")
