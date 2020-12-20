7ade68d237607b8129281e67796eafadc51136fa
tusharjain <tnjiitr@gmail.com>
2020-12-20 12:02:51 -0800

    Refreshed

---
+++ elpa/counsel-20201218.1218/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tusharjain/.emacs.d/elpa/counsel-20201218.1218/counsel.el  -*- no-byte-compile: t -*-
+(define-package "counsel" "20201218.1218" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "e43141df8a92995336585b401eb550a375349759" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
